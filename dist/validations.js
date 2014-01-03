76d597ff10fd9f109adb03bdd25852a4426b551a
Troy Whiteley <troy@somany.us>
2014-01-03 15:38:05 -0800

    Release 0.0.1

---
+++ dist/validations.js
@@ -0,0 +1,123 @@
+
+/*!
+ * validations - The best project ever.
+ * v0.0.1
+ * 
+ * copyright First + Third 2014
+ * MIT License
+*/
+(function(){
+  var root = this;
+
+  var alphaSize = function(text) {
+    var count = 0;
+    var lower, upper, number, symbol, symbol2;
+    lower = upper = number = symbol = symbol2 = false;
+    var other = '';
+    var i, c;
+
+    for(i = 0, c = text.length; i < c; i++) {
+      if(!lower && /[a-z]/.text(text)) {
+        count += 26;
+        lower = true;
+      } else if(!upper && /[A-Z]/.text(text)) {
+        count += 26;
+        upper = true;
+      } else if(!number && /\d/.test(text)) {
+        count += 10;
+        number = true;
+      } else if(!symbol && '!@#$%^&*()'.indexOf(text) > -1) {
+        count += 10;
+        symbol = true;
+      } else if(!symbol2 && '~`-_=+[]{}\\|;:\'",.<>?/'.indexOf(text) > -1) {
+        count += 22;
+        symbol2 = true;
+      } else if(other.indexOf(text) === -1) {
+        count++;
+        other += text;
+      }
+    }
+
+    return count;
+  };
+
+  var validations = function(customValidators) {
+    var validator;
+
+    if(typeof customValidators === 'object') {
+      for(validator in customValidators) {
+        if(!valid[validator]) {
+          validations[validator] = customValidators[validator];
+        }
+      }
+    }
+
+    return this;
+  };
+
+  validations.validate = function(input, pattern, type) {
+    var result = {
+      valid: false,
+      type: type || 'validate'
+    };
+
+    if(pattern instanceof RegExp) {
+      result.valid = pattern.test(input);
+    } else if(typeof pattern === 'string') {
+      result.valid = pattern === input;
+    } else if(typeof pattern === 'function') {
+      result.valid = pattern(input);
+    }
+
+    return result;
+  };
+
+  validations.required = function(input) {
+    return valid.validate(input, function(text) {
+      return text.length > 0;
+    }, 'required');
+  };
+
+  validations.email = function(input) {
+    return valid.validate(input, /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/, 'email');
+  };
+
+  validations.password = function(input, method) {
+    method = method || function(text) {
+      return text.length > 5;
+    };
+
+    return valid.validate(input, method, 'password');
+  };
+
+  validations.passwordConfirm = function(input, confirm) {
+    var target;
+
+    return valid.validate(input, confirm, 'password-confirm');
+  };
+
+  validations.passwordStrength = function(password, secureThreshold) {
+    if(!password.length) return 0;
+
+    // Sets default threshold to NIST (SP 800-63) recommended 80bit.
+    secureThreshold = secureThreshold || 80;
+
+    var entropy = (password.length * Math.log(alphaSize(password))) / Math.log(2);
+    var bits = Math.round(entropy * 100) / 100;
+
+    if(bits > secureThreshold) bits = secureThreshold;
+
+    return ~~((bits / secureThreshold) * 100);
+  };
+
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = validations;
+    }
+    exports.validations = validations;
+  } else {
+    root.validations = validations;
+  }
+
+
+}());
